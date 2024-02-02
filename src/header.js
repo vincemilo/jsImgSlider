2b64c99cb61f13b9927447e33203079ff83778c5
Vince Milo <1554999+vincemilo@users.noreply.github.com>
2024-02-02 16:58:06 -0700

    initial commit, refresher on webpack

--- src/header.js
+++ src/header.js
@@ -1,8 +1,8 @@
 export default function header() {
-    const headerDiv = document.createElement('header');
-    const logo = document.createElement('div');
-    logo.className = 'logo';
-    logo.innerText = 'To Do App';
-    headerDiv.appendChild(logo);
-    return headerDiv;
-};
+    const headerDiv = document.createElement('header')
+    const logo = document.createElement('div')
+    logo.className = 'logo'
+    logo.innerText = 'To Do App'
+    headerDiv.appendChild(logo)
+    return headerDiv
+}
--- src/index.js
+++ src/index.js
@@ -1,37 +1,22 @@
-import header from "./header";
-import displayProjects from "./displayProjects";
-import addTaskToProject from "./addTaskToProject";
-import Task from "./task";
+import _ from 'lodash';
+import header from './header';
 import './style.css';
-import newBtn from "./newBtn";
-import footer from "./footer";
-import { projects } from "./projects"
+import footer from './footer';
 
-if (localStorage.length > 0){
-    for (const [key, value] of Object.entries(localStorage)) {
-        projects.setProjects(JSON.parse(value));
-      };
-};
-
-const content = document.getElementById('content');
+const content = document.createElement('content');
+document.body.appendChild(content);
 const main = document.createElement('main');
-const wrapper = document.createElement('div');
-wrapper.id = 'wrapper';
 
-wrapper.appendChild(header());
-wrapper.appendChild(main);
-content.appendChild(wrapper);
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+content.appendChild(header());
+main.appendChild(component());
+content.appendChild(main);
 content.appendChild(footer());
-
-// const task = new Task('Lorem ipsum', '2020-10-10', 'Uncategorized', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
-// const task2 = new Task('blahdsf', '2020-10-13', 'New Project', 'descrup2', 'on');
-// const task3 = new Task('blahdsf', '2020-10-14', 'Uncategorized', 'descrup3');
-// const task4 = new Task('blah', '2020-10-10', 'Uncategorized', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
-
-// addTaskToProject('Uncategorized', task);
-// addTaskToProject('New Project', task2);
-// addTaskToProject('Uncategorized', task3);
-// addTaskToProject('Uncategorized', task4);
-
-displayProjects();
-newBtn();
